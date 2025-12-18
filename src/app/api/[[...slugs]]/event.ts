24857a89269e37f0cc3cfd4b5511f472a216f928
btmxh <ngoduyanh.chip@gmail.com>
2025-12-18 23:55:02 +0700

    feat: add basic qr checkin/out

--- src/app/api/[[...slugs]]/event.ts
+++ src/app/api/[[...slugs]]/event.ts
@@ -1,21 +1,48 @@
 import { auth } from "@/lib/auth";
 import Elysia from "elysia";
-import { unauthorized } from "./perms";
+import { checkPerm, unauthorized } from "./perms";
 import { createHmac, randomBytes } from "crypto";
 import { QRCODE_TIMEOUT, QRPayload, QRSigner } from "@/lib/qr";
+import z from "zod";
+import { db } from "@/lib/db";
 
-const qrSigner = await QRSigner.fromSecret(process.env.QR_SIGNING_SECRET!);
+const qrSigner = await QRSigner.fromPrivateKey(
+  process.env.QR_SIGNING_PRIVATE_KEY!,
+  process.env.NEXT_PUBLIC_QR_SIGNING_PUBLIC_KEY!,
+);
 
-export const eventsRouter = new Elysia({ prefix: "/events" }).get(
-  "/qrcode",
-  async ({ request: { headers }, status, set }) => {
+export const eventsRouter = new Elysia({ prefix: "/events" })
+  .get("/qrcode", async ({ request: { headers }, status, set }) => {
     set.headers["Cache-Control"] = "no-store";
 
     const session = await auth.api.getSession({ headers });
     if (!session) return unauthorized(status);
-    const token = qrSigner.generateToken(session.user.id);
-    return {
-      url: `https://gymembrace.app/checkin?token=${token}`,
-    };
-  },
-);
+    const url = await qrSigner.generateUrl(
+      session,
+      "https://gymembrace.app/qr",
+    );
+    return { url };
+  })
+  .post(
+    "/new",
+    async ({ body: { mode, url }, status, request: { headers } }) => {
+      await checkPerm(headers, status, { events: ["create"] });
+      try {
+        const { userId } = await qrSigner.verifyUrl(url);
+        const res = await db.collection("events").insertOne({
+          userId,
+          mode,
+          createdAt: new Date(),
+        });
+        return { docId: res.insertedId };
+      } catch (err) {
+        return status(400, { message: `${err}` });
+      }
+    },
+    {
+      body: z.object({
+        mode: z.enum(["check-in", "check-out"]),
+        url: z.string().min(1),
+      }),
+    },
+  );
--- src/app/api/[[...slugs]]/perms.ts
+++ src/app/api/[[...slugs]]/perms.ts
@@ -5,6 +5,7 @@ import { status as elysiaStatus } from "elysia";
 type RoomPerm = (typeof statement)["rooms"][number];
 type EquipmentPerm = (typeof statement)["equipments"][number];
 type FeedbackPerm = (typeof statement)["feedbacks"][number];
+type EventPerm = (typeof statement)["events"][number];
 
 // undefined: no session or no permission
 // null: no session, but still authorized as guest
@@ -16,6 +17,7 @@ export const checkPerm = async (
     rooms?: RoomPerm[];
     equipments?: EquipmentPerm[];
     feedbacks?: FeedbackPerm[];
+    events?: EventPerm[];
   },
 ) => {
   const session = await auth.api.getSession({ headers });
